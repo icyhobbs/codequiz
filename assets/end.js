3bcb0d00607459be20e74a304ea7ea1519d19083
Vincent Hobbs <trackvinny@gmail.com>
2021-11-30 23:30:11 -0500

    update

--- assets/end.js
+++ assets/end.js
@@ -28,6 +28,6 @@ username.addEventListener("keyup", () =>{
       highScores.splice(3)
 
       localStorage.setItem("highScores", JSON.stringify(highScores))
-      window.location.assign("/")
+      window.location.assign("./highscores.html")
 
  }
